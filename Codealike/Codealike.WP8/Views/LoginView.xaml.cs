0ce2bb1e2b2c8eb21c7acc9bd2ea2f0fcec18685
bujdeabogdan <bujdeabogdan@gmail.com>
2014-04-09 01:58:19 +0300

    finished design for login page

--- Codealike/Codealike.WP8/Views/LoginView.xaml.cs
+++ Codealike/Codealike.WP8/Views/LoginView.xaml.cs
@@ -1,10 +1,40 @@
-﻿namespace Codealike.WP8
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Windows.System;
+
+namespace Codealike.WP8.Views
 {
     public partial class LoginView
     {
+        private bool _helpIsNotOpened;
+
         public LoginView()
         {
             InitializeComponent();
+            Loaded += LoginViewLoaded;
+        }
+
+        private void LoginViewLoaded(object sender, RoutedEventArgs e)
+        {
+            _helpIsNotOpened = true;
+        }
+
+
+        private void OnHelpTapped(object sender, GestureEventArgs e)
+        {
+            if (_helpIsNotOpened)
+            {
+                HelpAnimation.Begin();
+            }
+            else
+                HelpText.Opacity = 0;
+            _helpIsNotOpened = !_helpIsNotOpened;
+        }
+
+        private void OpenSettingsPage(object sender, GestureEventArgs e)
+        {
+            Launcher.LaunchUriAsync(new Uri("https://codealike.com/Settings/Account"));
         }
     }
 }
