d068a99fab252caddd7d5396b9ae1c166b2c598e
bujdeabogdan <bujdeabogdan@gmail.com>
2014-04-10 10:09:50 +0300

    added loading animation to user data

--- Codealike/Codealike.WP8/Views/UserDataView.xaml.cs
+++ Codealike/Codealike.WP8/Views/UserDataView.xaml.cs
@@ -1,7 +1,9 @@
-﻿using Codealike.WP8.ViewModels;
-
-namespace Codealike.WP8.Views
+﻿namespace Codealike.WP8.Views
 {
+    using System.Threading.Tasks;
+    
+    using ViewModels;
+
     public partial class UserDataView
     {
         private IUserDataViewModel _viewModel;
@@ -17,11 +19,25 @@ namespace Codealike.WP8.Views
             InitializeUI();
         }
 
-        private void InitializeUI()
-        {            
-            CodingBar.Width = _viewModel.UserData.ActivityPercentage.Coding * 3;
+        private async void InitializeUI()
+        {
+            CodingBar.Width = 0;
+            DebuggingBar.Width = 0;
+            BuildingBar.Width = 0;
+            for ( int i = 0; i < 100; i++ )
+            {
+                CodingBar.Width += ( _viewModel.UserData.ActivityPercentage.Coding * 3 ) / 100;
+                DebuggingBar.Width += ( _viewModel.UserData.ActivityPercentage.Debugging * 3 ) / 100;
+                BuildingBar.Width += ( _viewModel.UserData.ActivityPercentage.Building * 3 ) / 100;
+
+                CodingPercentage.Text = ( ( _viewModel.UserData.ActivityPercentage.Coding * i ) / 100 ).ToString("F");
+                DebuggingPercentage.Text = ( ( _viewModel.UserData.ActivityPercentage.Debugging * i ) / 100 ).ToString("F");
+                BuildingPercentage.Text = ( ( _viewModel.UserData.ActivityPercentage.Building * i ) / 100 ).ToString("F");
+                await Task.Delay(1);
+            }
+            /*CodingBar.Width = _viewModel.UserData.ActivityPercentage.Coding * 3;
             DebuggingBar.Width = _viewModel.UserData.ActivityPercentage.Debugging * 3;
-            BuildingBar.Width = _viewModel.UserData.ActivityPercentage.Building * 3;
+            BuildingBar.Width = _viewModel.UserData.ActivityPercentage.Building * 3;*/
         }
     }
 }
