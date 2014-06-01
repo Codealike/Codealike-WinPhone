161eaaf1bb6198d4f092114b083e354befc0ba89
bujdeabogdan <bujdeabogdan@gmail.com>
2014-06-01 06:53:55 +0300

    released version 1.1

--- Codealike/Codealike.WP8/ViewModels/LoginViewModel.cs
+++ Codealike/Codealike.WP8/ViewModels/LoginViewModel.cs
@@ -22,7 +22,7 @@
             _pageNavigationService = pageNavigationService;
             _userNotificationService = userNotificationService;
             _appRepository = appRepository;
-            TokenData = "bogdan/aa8d8752-7404-46ab-b55f-4b40fc8b60e7";
+            //TokenData = "bogdan/aa8d8752-7404-46ab-b55f-4b40fc8b60e7";
         }
 
 
